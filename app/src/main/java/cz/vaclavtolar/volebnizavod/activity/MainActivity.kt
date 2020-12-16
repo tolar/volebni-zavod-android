d6e4efedb1aba67de9d0957429a6255606e63f7c
Vaclav Tolar <vaclav.tolar@gmail.com>
2020-12-16 23:15:03 +0100

    Elections buttons

--- app/src/main/java/cz/vaclavtolar/volebnizavod/activity/MainActivity.kt
+++ app/src/main/java/cz/vaclavtolar/volebnizavod/activity/MainActivity.kt
@@ -1,7 +1,11 @@
 package cz.vaclavtolar.volebnizavod.activity
 
 import android.os.Bundle
+import android.util.Log
+import android.view.View
+import android.view.ViewGroup
 import androidx.appcompat.app.AppCompatActivity
+import androidx.recyclerview.widget.RecyclerView
 import cz.vaclavtolar.volebnizavod.R
 import cz.vaclavtolar.volebnizavod.dto.Election
 import cz.vaclavtolar.volebnizavod.service.ServerService
@@ -18,8 +22,12 @@ class MainActivity : AppCompatActivity() {
     override fun onStart() {
         super.onStart()
         val call: Call<List<Election>> = ServerService.getInstance().allElections
-        call.enqueue(object: Callback < List < Election > > {
-            override fun onResponse(call: Call<List<Election>>, response: Response<List<Election>>) {
+        call.enqueue(object : Callback<List<Election>> {
+            override fun onResponse(
+                call: Call<List<Election>>,
+                response: Response<List<Election>>
+            ) {
+                Log.d("Data from server: {}", response.message())
                 //TODO("Not yet implemented")
             }
 
@@ -27,5 +35,25 @@ class MainActivity : AppCompatActivity() {
                 //TODO("Not yet implemented")
             }
         })
+    }
+
+    class ElectionsAdapter : RecyclerView.Adapter<ElectionsAdapter.ViewHolder>() {
+
+        override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder {
+            TODO("Not yet implemented")
+        }
+
+        override fun onBindViewHolder(holder: ViewHolder, position: Int) {
+            TODO("Not yet implemented")
+        }
+
+        override fun getItemCount(): Int {
+            TODO("Not yet implemented")
+        }
+
+        class ViewHolder : RecyclerView.ViewHolder {
+            constructor(itemView: View): super(itemView)
+        }
+
     }
 }
